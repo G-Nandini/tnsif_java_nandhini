299a17d536f534119be07d16b90d98c606ea823d
Lenovo <Lenovo@DESKTOP-VK0QRSV>
2025-10-08 15:10:36 +0530

    commit 2

---
+++ tnsif_java_nandhini/src/wrapperclass/wrapperclassdemo.java
@@ -0,0 +1,20 @@
+package wrapperclass;
+
+public class WrapperClassDemo {
+
+	public static void main(String[] args) {
+	
+		
+		int x = 5;
+		//autoboxing
+		Integer y = Integer.valueOf(x);
+		
+		//unboxing 
+		
+		int z = y;
+		
+		System.out.println(y +" "+ z);
+
+	}
+
+}
