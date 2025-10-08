98a5ed33e2e6f751318a7d86a4ab6ddc2d89c754
Lenovo <Lenovo@DESKTOP-VK0QRSV>
2025-10-08 15:05:50 +0530

    commit 1

---
+++ tnsif_java_nandhini/src/com/nandhini/assignment/employees/Employee.java
@@ -0,0 +1,44 @@
+package com.nandhini.assignment.employees;
+
+public class Employee {
+        private int employeeId;
+        private String name;
+        private  int salary;
+		/**
+		 * @return the employeeId
+		 */
+		public int getEmployeeId() {
+			return employeeId;
+		}
+		/**
+		 * @param employeeId the employeeId to set
+		 */
+		public void setEmployeeId(int employeeId) {
+			this.employeeId = employeeId;
+		}
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
+		}
+		/**
+		 * @param name the name to set
+		 */
+		public void setName(String name) {
+			this.name = name;
+		}
+		/**
+		 * @return the salary
+		 */
+		public int getSalary() {
+			return salary;
+		}
+		/**
+		 * @param salary the salary to set
+		 */
+		public void setSalary(int salary) {
+			this.salary = salary;
+		}
+        
+}
